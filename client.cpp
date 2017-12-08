6c92b7b9b2553e3da0ca1f81e573a2e8fa33b703
AadityaJ <aadityajamuar@gmail.com>
2017-12-08 14:05:16 +0530

    added some basic stuff

--- client.cpp
+++ client.cpp
@@ -1,6 +1,25 @@
 #include "includes/mClient.h"
-
+#define VNAME(x) #x
+/*class share{
+    string str;
+public:
+    share(string str){this->str=str;}
+    string to_str(){
+        return str;
+    }
+};*/
 int main(int argc, char const *argv[]) {
     Client c(8000);
+    int a=5;
+    double b=15.00;
+    string snd(VNAME(a));
+    snd+=(","+to_string(a));
+    c.Send(snd);
+    string snd2(VNAME(b));
+    snd2+=(","+to_string(b));
+    Client c2(8000);
+    c2.Send(snd2);
+    Client c3(8000);
+    c3.Send(snd2);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,8 +1,33 @@
 #include "includes/mServer.h"
 #include "includes/state.h"
+class shareDS{
+    string val;
+public:
+    shareDS(string val){this->val=val;}
+    string to_str(){
+        return val;
+    }
+};
+pair<string,string> split(string a){
+	string temp1="",temp2="";
+	int i=0;
+	for(;a[i]!=',';i++){temp1.push_back(a[i]);}
+	i++;
+	for(;i!=a.length();i++){temp2.push_back(a[i]);}
+	return make_pair(temp1,temp2);
+}
 int main(int argc, char const *argv[]) {
     Server s;
     s.listen_(8000);
-    
+    state<shareDS,string> st;
+    while(1){
+        string recv=s.receive();
+        //cout<<recv<<endl;
+        pair<string,string>prv=split(recv);
+        shareDS obj(prv.first);
+        cout<<"ADDED: "<<obj.to_str()<<" "<<prv.second<<endl;
+        st.add(obj,prv.second);
+        //cout<<st.seek(obj);
+    }
     return 0;
 }
